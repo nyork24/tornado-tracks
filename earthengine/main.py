eaaee925a57c1d60e7ee2b240d56295eaca11dab
nyork24 <nolanblox@gmail.com>
2024-12-31 15:06:40 -0500

    adding ee imagery

---
+++ earthengine/main.py
@@ -0,0 +1,20 @@
+import ee
+
+service_account = "local-host@ee-tornado-tracks.iam.gserviceaccount.com"
+credentials = ee.ServiceAccountCredentials(service_account, "C:\Users\Lenovo\earth-engine-api-key.json")
+ee.Initialize(credentials)
+
+def get_center(lat1, lon1, lat2, lon2):
+    center_lat = (lat1 + lat2) / 2
+    center_lon = (lon1 + lon2) / 2
+    return (center_lat, center_lon)
+
+
+# coordinates = 
+# point = ee.Geometry.Point()
+
+def main():
+    print(get_center(35.284,-97.628,35.341,-97.3999))
+
+main()
+    
