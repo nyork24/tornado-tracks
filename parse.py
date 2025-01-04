15c18eb5c6e873742092824d11497ea24828d6ea
Nolan <nolan@WalterKballsMBP.lan>
2025-01-04 12:17:21 -0500

    dataframe parsing

--- parse.py
+++ parse.py
@@ -1,14 +1,27 @@
 import pandas as pd
-from flask import Flask
+import numpy as np
 
 # Load CSV
-data = pd.read_csv('2022_torn.csv')
+data = pd.read_csv('2022_torn.csv', index_col='om', usecols=['om', 'date', 'time', 'st', 'mag', 'loss', 'slat', 'slon', 'elat', 'elon', 'len', 'wid'])
 
 # Example: Parse and clean
 data['start_coords'] = list(zip(data['slat'], data['slon']))
 data['end_coords'] = list(zip(data['elat'], data['elon']))
-data['date'] = pd.to_datetime(data['date'])
-data['time'] = pd.to_datetime(data['time'])
+#data.drop(['slat', 'slon', 'elat', 'elon'], axis=1, inplace=True)
 
-app = Flask(__name__)
 
+"""
+data[column label] - returns a series equivelent to the data within the column in each entry
+data.loc[row value] - returns a series equivelent to the data in each entry matching the row label
+"""
+
+def main():
+    print(data)
+    print(data['len'])
+    print(data.loc[620940])
+    only_ef3 = data[data.mag.isin([3])]
+    print(only_ef3)
+
+
+
+main()
