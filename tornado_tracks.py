f8bb09f4c6caac750a88d1fc47c62fae3918e945
Nolan <nolan@Walter-Kimballs-MacBook-Pro.local>
2025-01-04 13:23:36 -0500

    added before and after image routes to flask api

--- tornado_tracks.py
+++ tornado_tracks.py
@@ -3,7 +3,7 @@ import pandas as pd
 from earthengine.mosiac import *
 
 app = Flask(__name__)
-data = pd.read_csv('2022_torn.csv', index_col='om', usecols=['om', 'date', 'time', 'st', 'mag', 'loss', 'slat', 'slon', 'elat', 'elon', 'len', 'wid'])
+data = pd.read_csv('2022_torn.csv', index_col='om', usecols=['om', 'yr', 'mo', 'dy', 'time', 'st', 'mag', 'loss', 'slat', 'slon', 'elat', 'elon', 'len', 'wid'])
 data['start_coords'] = list(zip(data['slat'], data['slon']))
 data['end_coords'] = list(zip(data['elat'], data['elon']))
 
@@ -11,8 +11,32 @@ data['end_coords'] = list(zip(data['elat'], data['elon']))
 def index():
     return "<p>Tornado Tracks</p>"
 
-@app.route("/satellite/<int:id>")
-def get_image():
-    img_link_before = ""
-    img_link_after = ""
-    return '<img src=' + img_link_before + '>' + '<img src=' + img_link_after + '>'
+@app.route("/satellite/before/<int:id>")
+def image():
+    tornado_data = data[data.om.isin([id])]
+    dy = tornado_data.iloc[0]['dy']
+    mo = tornado_data.iloc[0]['mo']
+    yr = tornado_data.iloc[0]['yr']
+    lat1 = tornado_data.iloc[0]['slat']
+    lon1 = tornado_data.iloc[0]['slon']
+    lat2 = tornado_data.iloc[0]['elat']
+    lon2 = tornado_data.iloc[0]['elon']
+
+    img_link_before = get_before_image(dy, mo, yr, lat1, lon1, lat2, lon2)
+
+    return '<img src=' + img_link_before + '>'
+
+@app.route("/satellite/after/<int:id>")
+def image():
+    tornado_data = data[data.om.isin([id])]
+    dy = tornado_data.iloc[0]['dy']
+    mo = tornado_data.iloc[0]['mo']
+    yr = tornado_data.iloc[0]['yr']
+    lat1 = tornado_data.iloc[0]['slat']
+    lon1 = tornado_data.iloc[0]['slon']
+    lat2 = tornado_data.iloc[0]['elat']
+    lon2 = tornado_data.iloc[0]['elon']
+
+    img_link_after = get_after_image(dy, mo, yr, lat1, lon1, lat2, lon2)
+
+    return '<img src=' + img_link_after + '>'
